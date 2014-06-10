7b1bd41f2ff27f8398686910e771a52e2801d021
yogalakshmi <yogalakshmiravi93@gmail.com>
2014-06-10 18:46:09 +0530

    title helper

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
+	 respond_to do |format|
+      format.html # home.html.erb
+      format.json { render json: @users }
+    end
   end
 
   def help
   end
 
   def about
+  end
+
+  def contact
   end
 end
